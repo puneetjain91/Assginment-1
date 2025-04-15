b26e80f91da8ad2fffc6df6381a45d082bc6d849
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2025-04-16 00:11:58 +0530

    Create variables.tf
    Creating the Variables-tf file for  RG module

---
+++ Terraform/modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for the resource group"
+}
