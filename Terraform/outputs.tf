c94db417bc4ba65a8795ba14ea2c6fb260c596b2
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2025-04-16 00:53:01 +0530

    Update outputs.tf
    # Output values for the project# outputs.tf: Defines output values for the project

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,1 +1,31 @@
- # Output values for the project
+ # Output values for the project# outputs.tf: Defines output values for the project
+
+# Resource Group Output
+output "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  value       = module.resource_group.resource_group_name
+}
+
+# Virtual Network Output
+output "virtual_network_name" {
+  description = "The name of the Virtual Network"
+  value       = module.vnet.virtual_network_name
+}
+
+# AKS Cluster Output
+output "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  value       = module.aks.aks_cluster_name
+}
+
+# Azure Key Vault Output
+output "key_vault_name" {
+  description = "The name of the Azure Key Vault"
+  value       = module.key_vault.key_vault_name
+}
+
+# Storage Account Output
+output "storage_account_name" {
+  description = "The name of the Azure Storage Account for Terraform state"
+  value       = var.storage_account_name
+}
